5ea3f075681c0ef05baa91426ed2b4a89e8be3c3
Chris Alexander <chris.alexander@adhocteam.us>
2024-09-10 14:44:07 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Christopher Michael Alexander on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
